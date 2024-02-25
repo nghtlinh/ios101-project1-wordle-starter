4e00e31a044b1810a793a55aad58092dc77a4e22
Monica Nguyen-Hoang <nguyenhl@dickinson.edu>
2024-02-25 16:03:55 -0500

    Finished ex 7

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -45,7 +45,7 @@ class BoardController: NSObject,
       // Tip: Use the transform property of the cell. Use transform.scaledBy to modify the scale. This should feel familiar to lab 1.
       // Checkpoint: After finishing this exercise, you should now be able to see that the board animates whenever you enter a new letter! If it's not animating, check your work on this exercise.
       // START YOUR CODE HERE
-      // ...
+        cell.transform = cell.transform.scaledBy(x: 1.05, y: 1.05)
       // END YOUR CODE HERE
     }, completion: { finished in
       cell.transform = CGAffineTransformIdentity
